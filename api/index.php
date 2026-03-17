274950bb898a6bcfcbc8a76966647dfdb3c2a986
Miguel <1bcmigueliglesias@gmail.com>
2026-03-17 13:39:52 +0100

    ahora he puesto las cabeceras de cors para que funcione las peticiones (es la primera modificacion de cors asi que no se si saldra bien)

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@ ob_start();
 // Headers CORS
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 header('Content-Type: application/json; charset=UTF-8');
 
 // Manejar preflight requests (OPTIONS)
